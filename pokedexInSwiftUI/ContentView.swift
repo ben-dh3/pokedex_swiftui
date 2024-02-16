fd2b48efcb386f9a75ef2bcd6b20808c8faa848b
ben-dh3 <benjaminduffield1997@gmail.com>
2024-02-16 16:48:16 +0000

    updates

--- pokedexInSwiftUI/ContentView.swift
+++ pokedexInSwiftUI/ContentView.swift
@@ -46,55 +46,51 @@ struct ContentView: View {
                         .padding(10)
                     HStack {
                         Section(header: Text("Search a Pokemon:")) {
-                            TextField("Enter Pokemon name", text: $search, onEditingChanged: { _ in
-                                self.search = self.search.lowercased()
-                            })
-                            .disableAutocorrection(true)
-                            .textInputAutocapitalization(.never)
+                            TextField("Enter Pokemon name", text: $search)
                             .onSubmit {
                                 guard !search.isEmpty else { return }
                                 viewModel.fetchPokemon(by: search)
-                                
                             }
                         }.padding()
                     }
                     
-                    
-                    if let pokemonName = viewModel.pokemonName {
-                        if let pokemonHeight = viewModel.pokemonHeight {
-                            if let pokemonWeight = viewModel.pokemonWeight {
-                                if let pokemonImage = viewModel.pokemonImage {
-                                    AsyncImage(url: URL(string: pokemonImage))
-                                    Text(pokemonName)
-                                        .font(Font.title3.weight(.bold))
+                    Section {
+                        if let pokemonName = viewModel.pokemonName {
+                            if let pokemonHeight = viewModel.pokemonHeight {
+                                if let pokemonWeight = viewModel.pokemonWeight {
+                                    if let pokemonImage = viewModel.pokemonImage {
+                                        AsyncImage(url: URL(string: pokemonImage))
+                                        Text(pokemonName)
+                                            .font(Font.title3.weight(.bold))
+                                            .padding(1)
+                                        Text("Height: \(pokemonHeight)")
+                                            .font(Font.body.weight(.medium))
+                                        Text("Weight: \(pokemonWeight)")
+                                            .font(Font.body.weight(.medium))
+                                    }
+                                }
+                            }
+                            Text("Types:")
+                                .font(Font.body.weight(.medium))
+                            HStack {
+                                ForEach(viewModel.pokemonType ?? [], id: \.self) { type in
+                                    Text(type)
                                         .padding(1)
-                                    Text("Height: \(pokemonHeight)")
-                                        .font(Font.body.weight(.medium))
-                                    Text("Weight: \(pokemonWeight)")
                                         .font(Font.body.weight(.medium))
                                 }
                             }
-                        }
-                        Text("Types:")
-                            .font(Font.body.weight(.medium))
-                        HStack {
-                            ForEach(viewModel.pokemonType ?? [], id: \.self) { type in
-                                Text(type)
-                                    .padding(1)
-                                    .font(Font.body.weight(.medium))
+                            Text("Abilities:")
+                                .font(Font.body.weight(.medium))
+                            HStack {
+                                ForEach(viewModel.pokemonAbility ?? [], id: \.self) { ability in
+                                    Text(ability)
+                                        .padding(1)
+                                        .font(Font.body.weight(.medium))
+                                }
                             }
+                        }else {
+                            Text("Loading...")
                         }
-                        Text("Abilities:")
-                            .font(Font.body.weight(.medium))
-                        HStack {
-                            ForEach(viewModel.pokemonAbility ?? [], id: \.self) { ability in
-                                Text(ability)
-                                    .padding(1)
-                                    .font(Font.body.weight(.medium))
-                            }
-                        }
-                    }else {
-                        Text("Loading...")
                     }
                 }
                 .onAppear {
